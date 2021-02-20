f4a14405e311e71cdeec36159b97bc08f2750e64
Michael Thompson <mikeylikes23@gmail.com>
2021-02-20 12:56:18 -0700

    Docker prod setup edits

--- docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# Building outside the container so making docker changes and rebuilding isn't slow
-
-rm -rf deploy
-mkdir deploy
-cp -r ../Omega/bin/Release/net5.0/publish/ ./deploy/
-
-docker build -t omega:1.0  .
--- docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-docker run -it --rm -p 5000:80 omega:1.0
-
-# Debug container
-# docker run --entrypoint "sh" -it omega:1.0
