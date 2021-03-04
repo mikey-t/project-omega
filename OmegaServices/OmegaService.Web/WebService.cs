a066a9deac5514a81c19a56595699f76937c0953
Mike Thompson <mikeylikes23@gmail.com>
2021-03-03 21:20:19 -0700

    Added OmegaService.Web. Commit before moving SPA and web controllers/startup logic into omega service.

---
+++ OmegaServices/OmegaService.Web/WebService.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OmegaPlumbing;
+
+namespace OmegaService.Web
+{
+  public class WebService : ProjectOmegaService
+  {
+    public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+    }
+
+    public override void ConfigureServices(IServiceCollection services, ILogger logger)
+    {
+    }
+  }
+}
