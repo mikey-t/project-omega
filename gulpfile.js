8a71ffb7ed4a3e6f1eedbf85222cf282a99d38db
Mike Thompson <mikeylikes23@gmail.com>
2021-06-13 13:51:32 -0600

    Replaced common gulp util functions with npm package @mikeyt23/gulp-utils

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
+// noinspection JSCheckFunctionSignatures
+
 const {src, dest, parallel, series} = require('gulp')
-const {spawn, spawnSync} = require('child_process')
+const {spawn} = require('child_process')
 const {argv} = require('yargs')
 const path = require('path')
 const rimraf = require('rimraf')
-const fs = require('fs')
 const fsp = require('fs').promises
-const which = require('which')
+const {
+  defaultSpawnOptions,
+  waitForProcess,
+  copyNewEnvValues,
+  overwriteEnvFile,
+  throwIfDockerNotRunning,
+  dockerContainerIsRunning,
+  bashIntoRunningDockerContainer
+} = require('@mikeyt23/gulp-utils')
 
 const clientAppPath = 'src/services/OmegaService.Web/client-app/'
 const serverAppPath = 'src/Omega/'
@@ -15,115 +24,37 @@ const dockerDirPath = 'deploy/docker/'
 const dockerProjectName = 'omega' // The docker project name option prevents warnings about unrelated orphaned containers
 const dockerDepsProjectName = 'omega_deps'
 const dockerDepsComposeName = 'docker-compose.deps.yml'
-const dockerNetworkName = 'omega-net'
 
-const spawnOptions = {
-  shell: true,
-  cwd: __dirname,
-  stdio: ['ignore', 'inherit', 'inherit'],
-}
-const spawnOptionsWithInput = {...spawnOptions, stdio: 'inherit'}
+const spawnOptions = {...defaultSpawnOptions}
 const dockerSpawnOptions = {...spawnOptions, cwd: path.resolve(__dirname, dockerDirPath)}
 const migratorSpawnOptions = {...spawnOptions, cwd: path.resolve(__dirname, dbMigratorPath)}
 const migratorSpawnOptionsWithInput = {...migratorSpawnOptions, stdio: 'inherit'}
 
-function waitForProcess(childProcess) {
-  return new Promise((resolve, reject) => {
-    childProcess.once('exit', (returnCode) => {
-      if (returnCode === 0) {
-        resolve(returnCode)
-      } else {
-        reject(returnCode)
-      }
-    })
-    childProcess.once('error', (err) => {
-      reject(err)
-    })
-  })
-}
-
-function getEnvDictionary(filePath) {
-  let dict = {}
-  fs.readFileSync(filePath).toString().split('\n').forEach(function (line) {
-    if (line && line.indexOf('=') !== -1) {
-      line = line.replace('\r', '').trim()
-      let parts = line.split('=')
-      dict[parts[0].trim()] = parts[1].trim()
-    }
-  })
-  return dict
-}
-
-async function copyEnv(templateDir, templateFilename, envDir, envFilename, overrideAll = true) {
-  await ensureEnvFile(templateDir, templateFilename, envDir, envFilename)
-
-  let templateDict = getEnvDictionary(templateDir + templateFilename)
-  let envDict = getEnvDictionary(envDir + envFilename)
-
-  // Determine what keys are missing from .env that are in template
-  let templateKeys = Object.keys(templateDict)
-  let envKeys = Object.keys(envDict)
-  let missingKeys = templateKeys.filter(k => !envKeys.includes(k))
-
-  if (missingKeys.length > 0) {
-    console.log(`Adding missing keys for ${envDir + envFilename}: `, missingKeys)
-  }
-
-  // Merge missing values with existing
-  let newEnvDict = {}
-  for (const [key, value] of Object.entries(overrideAll ? templateDict : envDict)) {
-    newEnvDict[key] = value
-  }
-  for (const key of missingKeys) {
-    newEnvDict[key] = templateDict[key]
-  }
-
-  // Sort
-  let newDictEntries = Object.entries(newEnvDict)
-  let newSortedEntries = newDictEntries.sort((a, b) => {
-    if (a < b) {
-      return -1
-    }
-    if (a > b) {
-      return 1
-    }
-    return 0
-  })
-
-  // Write to .env file
-  let newEnvFileContent = ''
-  for (let kvp of newSortedEntries) {
-    newEnvFileContent += `${kvp[0]}=${kvp[1]}\n`
-  }
-  await fsp.writeFile(envDir + envFilename, newEnvFileContent)
-}
-
-async function ensureEnvFile(templateDir, templateFilename, envDir, envFilename) {
-  const envTemplatePath = `${templateDir}${templateFilename}`
-  const envPath = `${envDir}${envFilename}`
-  if (!fs.existsSync(envPath)) {
-    console.log('Created new env file ' + envPath)
-    await fsp.copyFile(envTemplatePath, envPath)
-  }
-}
-
 async function syncEnvFiles() {
-  const rootTemplateDir = './'
-  const rootServerTemplateFilename = '.env.server.template'
-  const rootClientTemplateFilename = '.env.client.template'
-  const rootEnvDir = './'
-  const rootServerEnvFilename = '.env.server'
-  const rootClientEnvFilename = '.env.client'
+  const rootServerEnv = './.env.server'
+  const rootClientEnv = './.env.client'
 
   // Copy root .env.[category].template to .env
-  await copyEnv(rootTemplateDir, rootServerTemplateFilename, rootEnvDir, rootServerEnvFilename, false)
-  await copyEnv(rootTemplateDir, rootClientTemplateFilename, rootEnvDir, rootClientEnvFilename, false)
+  await copyNewEnvValues(`${rootServerEnv}.template`, rootServerEnv)
+  await copyNewEnvValues(`${rootClientEnv}.template`, rootClientEnv)
+  
 
   // Copy root .env.[category] to subdirectory .env files
-  await copyEnv(rootTemplateDir, rootServerEnvFilename, serverAppPath, '.env')
-  await copyEnv(rootTemplateDir, rootServerEnvFilename, dockerDirPath, '.env')
-  await copyEnv(rootTemplateDir, rootServerEnvFilename, dbMigratorPath, '.env')
-  await copyEnv(rootTemplateDir, rootClientEnvFilename, clientAppPath, '.env')
+  await overwriteEnvFile(rootServerEnv, path.join(serverAppPath, '.env'))
+  await overwriteEnvFile(rootServerEnv, path.join(dockerDirPath, '.env'))
+  await overwriteEnvFile(rootServerEnv, path.join(dbMigratorPath, '.env'))
+  await overwriteEnvFile(rootClientEnv, path.join(clientAppPath, '.env'))
+}
+
+async function deleteEnvFiles() {
+  await Promise.all([
+    fsp.unlink(path.join('./.env.server')),
+    fsp.unlink(path.join('./.env.client')),
+    fsp.unlink(path.join(serverAppPath, '.env')),
+    fsp.unlink(path.join(dockerDirPath, '.env')),
+    fsp.unlink(path.join(dbMigratorPath, '.env')),
+    fsp.unlink(path.join(clientAppPath, '.env'))
+  ])
 }
 
 async function yarnInstallClientApp() {
@@ -151,60 +82,36 @@ async function yarnBuild() {
   return waitForProcess(spawn('yarn', args, spawnOptions))
 }
 
-async function ensureDockerIsRunning() {
-  if (!which.sync('docker')) {
-    throw Error('docker was not found')
-  }
-
-  let childProcess = spawnSync('docker', ['info'], {encoding: 'utf8'})
-  if (childProcess.error) {
-    throw childProcess.error
-  }
-  if (!childProcess.stdout || childProcess.stdout.includes('ERROR: error during connect')) {
-    throw Error('docker is not running')
-  }
+async function dockerBashRunningContainer() {
+  await throwIfDockerNotRunning()
+  await bashIntoRunningDockerContainer(`${dockerDepsProjectName}_${argv['imageName']}`)
 }
 
-async function dockerBash() {
-  await ensureDockerIsRunning()
-  const args = ['run', '--rm', '--entrypoint', '"bash"', '-it', `omega_${argv['imageName']}:1.0`]
-  return waitForProcess(spawn('docker', args, spawnOptionsWithInput))
-}
-
-// We're assuming that deps isn't up by the lack of the network created in docker-compose.deps.yml
+// We're assuming that dependencies aren't up if the mssql container isn't running
 async function throwIfDockerDepsNotUp() {
-  await ensureDockerIsRunning()
-  let childProcess = spawnSync('docker', ['network', 'ls'], {encoding: 'utf8'})
-  if (childProcess.error) {
-    throw childProcess.error
-  }
-  console.log('Docker networks:')
-  console.log(childProcess.stdout)
-  if (!childProcess.stdout || !childProcess.stdout.includes(dockerNetworkName)) {
-    console.error(`ERROR: docker network ${dockerNetworkName} not found`)
-    throw `docker network ${dockerNetworkName} not found, create using "yarn run dockerDepsUp" or "yarn run dockerDepsUpDetached"`
-  } else {
-    console.log(`docker network ${dockerNetworkName} found`)
+  const mssqlIsRunning = await dockerContainerIsRunning('omega_mssql')
+  if (!mssqlIsRunning) {
+    throw 'Docker dependencies are not running'
   }
 }
 
 async function dockerBuild() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerProjectName, 'build', '--no-cache'], dockerSpawnOptions))
 }
 
 async function dockerUp() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerProjectName, 'up'], dockerSpawnOptions))
 }
 
 async function dockerDown() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerProjectName, 'down'], dockerSpawnOptions))
 }
 
 async function dockerStop() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerProjectName, 'stop'], dockerSpawnOptions))
 }
 
@@ -223,35 +130,35 @@ async function copyPublishedToDockerDir() {
 }
 
 async function dockerStandaloneBuild() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   const args = ['build', '-f', 'Dockerfile.standalone', '-t', 'omega_standalone:1.0', '.']
   return waitForProcess(spawn('docker', args, dockerSpawnOptions))
 }
 
 async function dockerStandaloneRun() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   // Note that running docker as a gulp command disallows passing the -t command since that would try to configure the terminal
   const args = ['run', '-i', '--rm', '-p', '5000:80', 'omega_standalone:1.0']
   return waitForProcess(spawn('docker', args, dockerSpawnOptions))
 }
 
 async function dockerDepsUp() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerDepsProjectName, '-f', dockerDepsComposeName, 'up'], dockerSpawnOptions))
 }
 
 async function dockerDepsUpDetached() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerDepsProjectName, '-f', dockerDepsComposeName, 'up', '-d'], dockerSpawnOptions))
 }
 
 async function dockerDepsDown() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerDepsProjectName, '-f', dockerDepsComposeName, 'down'], dockerSpawnOptions))
 }
 
 async function dockerDepsStop() {
-  await ensureDockerIsRunning()
+  await throwIfDockerNotRunning()
   return waitForProcess(spawn('docker-compose', ['--project-name', dockerDepsProjectName, '-f', dockerDepsComposeName, 'stop'], dockerSpawnOptions))
 }
 
@@ -304,7 +211,7 @@ exports.dockerDown = dockerDown
 exports.dockerStop = dockerStop
 
 // Run images with bash entrypoint to troubleshoot what files are ending up in the built images. Requires an arg --image-name to be passed. See package.json for examples.
-exports.dockerBash = dockerBash
+exports.dockerBash = dockerBashRunningContainer
 
 // Good for rapidly making docker-compose changes and testing them
 exports.dockerRecreate = series(throwIfDockerDepsNotUp, dockerDown, dockerUp)
@@ -330,3 +237,4 @@ exports.testDbMigrate = series(throwIfDockerDepsNotUp, parallel(syncEnvFiles, de
 
 // Create .env files if they don't exist and adds any new key value pairs. Also changes existing values in subdirectory .env files if non-template values have changed.
 exports.syncEnvFiles = syncEnvFiles
+exports.deleteEnvFiles = deleteEnvFiles
